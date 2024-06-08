75549b8bb2c5d5f809bfcc898590edad1ef4e574
himanshusalvekar07 <himanshusalvekar@gmail.com>
2024-06-08 13:42:20 +0530

    changes added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -21,6 +21,11 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -56,3 +61,6 @@ MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model2024"
+MODEL_PUSHER_S3_KEY = "model-registry"
